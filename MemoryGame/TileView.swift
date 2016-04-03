b4b2a4f0585b9c6626165e47cd3f0ef26f11b948
Benjamin Johnson <bkj986@uowmail.edu.au>
2016-04-03 20:58:22 +1000

    Added TileView

---
+++ MemoryGame/TileView.swift
@@ -0,0 +1,24 @@
+//
+//  TileView.swift
+//  MemoryGame
+//
+//  Created by Benjamin Johnson on 3/04/2016.
+//  Copyright © 2016 Benjamin Johnson. All rights reserved.
+//
+
+import UIKit
+
+class TileView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+   
+    
+    
+}
