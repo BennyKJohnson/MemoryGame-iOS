e6b639931dbe1248046b3481446459b018e9c1bf
BennyKJohnson <bkj986@uowmail.edu.au>
2016-04-04 10:15:10 +1000

    Implemented TileView

--- MemoryGame/TileView.swift
+++ MemoryGame/TileView.swift
@@ -8,17 +8,51 @@
 
 import UIKit
 
+protocol TileViewDelegate {
+    func didSelectTile(tileView: TileView)
+}
+
 class TileView: UIView {
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
-    }
-    */
-
-   
+    var imageView: UIImageView!
     
+    var image: UIImage!
+    
+    var defaultImage: UIImage? = UIImage(named: "question")
+    
+    var tileIndex: Int = -1
+    
+    func revealImage() {
+        imageView.image = image
+    }
+    
+    func coverImage() {
+        imageView.image = defaultImage
+    }
+    
+    func hide() {
+        imageView.image = nil
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        // Add ImageView
+        imageView = UIImageView(frame: CGRectMake(0,0,0,0))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+
+        addSubview(imageView)
+        
+        // Setup Constraints
+        let topConstraint = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: 0.0)
+        let bottomConstraint = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: 0.0)
+        let leftConstraint = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Leading, multiplier: 1.0, constant: 0.0)
+        let rightConstraint = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Trailing, multiplier: 1.0, constant: 0.0)
+        // Add Constraints
+        self.addConstraints([topConstraint, bottomConstraint, leftConstraint, rightConstraint])
+        
+        imageView.image = defaultImage
+        
+    }
     
 }
